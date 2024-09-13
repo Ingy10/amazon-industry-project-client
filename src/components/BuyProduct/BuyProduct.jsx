831458e8c5ce2c809755a6bac0e34df18c43fa5d
Nico Reincheld <nicoreincheld@gmail.com>
2024-09-13 11:18:02 -0400

    Put brnach on ice for now

--- src/components/BuyProduct/BuyProduct.jsx
+++ src/components/BuyProduct/BuyProduct.jsx
@@ -1,4 +1,5 @@
 import './BuyProduct.scss';
+import locationLogo from '../../assets/icons/location.png';
 
 function BuyProduct() {
   return (
@@ -10,13 +11,25 @@ function BuyProduct() {
         </p>
         <div className="buy-product__deliver-container">
           <img
-            src=""
+            src={locationLogo}
             alt="location svg"
             className="buy-product__deliver-logo"
           />
           <p className="buy-product__deliver-location">
             Select delivery location
           </p>
+        </div>
+        <select className="buy-product__quantity">
+          <option value="1">Quantity: 1</option>
+          <option value="2">Quantity: 2</option>
+          <option value="3">Quantity: 3</option>
+          <option value="4">Quantity: 4</option>
+        </select>
+        <div className="buy-product__add">
+          <p className="buy-product__add-text">Add to Cart</p>
+        </div>
+        <div className="buy-product__buy">
+          <p className="buy-product__buy-text">Buy Now</p>
         </div>
       </section>
     </>
